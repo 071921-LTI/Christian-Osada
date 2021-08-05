ef397684ca84426f7e95e33b011eb80064532e11
Christian Osada <christian.osada@lntinfotech.com>
2021-08-05 13:03:47 -0400

    Messed around with Ajax

---
+++ HTML/christian_osada_html/ajax.js
@@ -0,0 +1,87 @@
+// document.getElementById('getData').onclick = getData;
+document.getElementById('getData').addEventListener("click", getData);
+
+let apiURL = 'https://goweather.herokuapp.com/weather/Enfield';
+
+function getData() {
+
+    // Retrieving the user input, in this case the pokemon id to be retrieved
+    let userInput = document.getElementById('dataInput').value;
+
+    // 4 Steps to make an AJAX call
+    // 1: Create an XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+
+    // 2: Set a callback function to the readystatechange event
+    xhr.onreadystatechange = receiveData;
+
+    // 3: Open the request/parameterize it
+    xhr.open('GET', `${apiURL}`);
+
+    // 4: Send the request
+    xhr.send();
+
+    function receiveData(){
+        let dataSection = document.getElementById('data');
+
+        // Ready state is done, reponse has been retrieved
+        if(xhr.readyState === 4){
+            // Status code is in the 200s, meaning successful
+            if(xhr.status >= 200 && xhr.status < 300){
+                let response = xhr.responseText;
+                // Converting JSON to JS object
+                response = JSON.parse(response);
+                // Data processing behavior
+                populateData(response);
+            } else{
+                document.getElementById('data').innerHTML = "<img src=https://ih1.redbubble.net/image.731955024.9007/farp,small,wall_texture,product,750x1000.u3.jpg />";
+            }
+        }
+    }
+  
+}
+
+function populateData(response) {
+    console.log(response);
+    // Display pokemon information in the section tag of my html document
+    let dataSection = document.getElementById('data');
+   
+   // Resets the innerHTML before loading new data
+   dataSection.innerHTML ='';
+
+   let todayTag = document.createElement('h2');
+   todayTag.innerHTML = 'Today: ';
+   let tempTag = document.createElement('h3');
+   tempTag.innerHTML = response.temperature;
+   let windTag = document.createElement('h3');
+   windTag.innerHTML = response.wind;
+   let descTag = document.createElement('h3');
+   descTag.innerHTML = response.description;
+   
+   let forecastArray = response.forecast;
+   let forecasts = document.createElement('ul');
+
+   // Appending list elements to a ul
+   for(let forecast of forecastArray){
+       let forecastLi = document.createElement('li');
+       forecastLi.innerHTML = forecast.temperature;
+       forecasts.appendChild(forecastLi);
+   }
+
+   // Appending h3 and List to the section tag
+   dataSection.appendChild(todayTag);
+   dataSection.appendChild(tempTag);
+   dataSection.appendChild(windTag);
+   dataSection.appendChild(descTag);
+   dataSection.appendChild(forecasts);
+
+   // Appending sprites to section
+   let spritesObject = response.sprites;
+   for(let sprite in spritesObject){
+       if(spritesObject[sprite] && spritesObject[sprite].length > 2){
+           let spriteImg = document.createElement('img');
+           spriteImg.src = spritesObject[sprite];
+           dataSection.appendChild(spriteImg);
+       }
+   }
+}
