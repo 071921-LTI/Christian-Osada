7be8542795f82b8832dda04ca62080e386fabd66
Christian Osada <christian.osada@lntinfotech.com>
2021-08-12 17:03:14 -0400

    fixed user delete so you can now delete users that have reimbursements on record

--- project-1/project-1-java/src/main/java/com/one/models/Reimbursement.java
+++ project-1/project-1-java/src/main/java/com/one/models/Reimbursement.java
@@ -12,6 +12,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cascade;
+
 @Entity
 @Table(name = "ers_reimbursement",  schema = "project1")
 public class Reimbursement {
