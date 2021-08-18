769356f082a130e5af053e6228fdecac97278194
Christian Osada <christian.osada@lntinfotech.com>
2021-08-18 14:31:42 -0400

    employee homepage now displays users information

--- project-1/project-1-html-css/employee.js
+++ project-1/project-1-html-css/employee.js
@@ -20,45 +20,18 @@ header.append('Authorization', token);
     if(response.status >= 200 && response.status < 300){
         let data = await response.json();
         console.log(data)
-        //populateData(data);
+        populateUserInfo(data);
     } else{
         console.log('Unable to retrieve data.')
     }
 }
 
-function populateData(response) {
-
-
-//     let dataSection = document.getElementById('data');
-   
-//    // Resets the innerHTML before loading new data
-//    dataSection.innerHTML ='';
-
-//    let nameTag = document.createElement('h3');
-//    nameTag.innerHTML = response.name.toUpperCase();
-   
-//    let abilitiesArray = response.abilities;
-//    let abilities = document.createElement('ul');
-//    // Appending list elements to a ul
-//    for(let ability of abilitiesArray){
-//        let abilityLi = document.createElement('li');
-//        abilityLi.innerHTML = ability.ability.name;
-//        abilities.appendChild(abilityLi);
-//    }
-
-//    // Appending h3 and List to the section tag
-//    dataSection.appendChild(nameTag);
-//    dataSection.appendChild(abilities);
-
-//    // Appending sprites to section
-//    let spritesObject = response.sprites;
-//    for(let sprite in spritesObject){
-//        if(spritesObject[sprite] && spritesObject[sprite].length > 2){
-//            let spriteImg = document.createElement('img');
-//            spriteImg.src = spritesObject[sprite];
-//            dataSection.appendChild(spriteImg);
-//        }
-//    }
+function populateUserInfo(response) {
+    document.getElementById("idp").innerHTML = response.id;
+    document.getElementById("firstNamep").innerHTML = response.firstName;
+    document.getElementById("lastNamep").innerHTML = response.lastName;
+    document.getElementById("emailp").innerHTML = response.email;
+    document.getElementById("rolep").innerHTML = response.role.role;
 }
 
 function logout() {
